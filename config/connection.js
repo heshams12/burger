0465f72d2d948ec577e3fab13ca79df2b2a522eb
Unknown <Heshams12@hotmail.com>
2018-03-26 11:50:49 -0700

    updated env info.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+if (process.env.NODE_ENV !== 'production') 
+{
+  require('dotenv').load();
+}
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "hendrix1",
-  database: "burgers_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA
 });
 
 // Make connection.
